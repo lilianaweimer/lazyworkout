6f74245bf5124ea92fdc8e6273d7a929060a27f1
Liliana Weimer <lilianaweimer@Lilianas-MacBook-Pro-2.local>
2022-06-10 16:22:23 -0600

    add basic components

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,5 +1,97 @@
 const Options = () => {
-    return <h2>Options</h2>;
-  }
-  
-  export default Options;
+  return (
+    <section>
+      <h2>Options</h2>
+      <form>
+        <fieldset>
+          <legend>Body parts?? (idk what to call this yet)</legend>
+          <>
+            <input
+              type="checkbox"
+              id="legs"
+              name="legs"
+              value="checked"
+              className="options_checkbox"
+              checked
+            />
+            <label htmlFor="legs">Legs</label>
+          </>
+          <>
+            <input
+              type="checkbox"
+              id="core"
+              name="core"
+              value="checked"
+              className="options_checkbox"
+              checked
+            />
+            <label htmlFor="core">Core</label>
+          </>
+          <>
+            <input
+              type="checkbox"
+              id="arms"
+              name="arms"
+              value="checked"
+              className="options_checkbox"
+              checked
+            />
+            <label htmlFor="arms">Arms</label>
+          </>
+          <>
+            <input
+              type="checkbox"
+              id="yoga"
+              name="yoga"
+              value="checked"
+              className="options_checkbox"
+              checked
+            />
+            <label htmlFor="yoga">Yoga</label>
+          </>
+        </fieldset>
+        <fieldset>
+          <legend>Tools?</legend>
+          <>
+            <input
+              type="checkbox"
+              id="pilates_ring"
+              name="pilates_ring"
+              value=""
+              className="options_checkbox"
+            />
+            <label htmlFor="pilates_ring">Pilates Ring</label>
+          </>
+          <>
+            <input
+              type="checkbox"
+              id="weights"
+              name="weights"
+              value=""
+              className="options_checkbox"
+            />
+            <label htmlFor="weights">Weights</label>
+          </>
+        </fieldset>
+        <fieldset>
+          <legend>Intensity</legend>
+          <>
+            <input
+              type="range"
+              id="intensity"
+              name="intensity"
+              className="options_checkbox"
+              min="1"
+              max="3"
+              step="1"
+              value="1"
+            />
+            <label htmlFor="intensity">Intensity</label>
+          </>
+        </fieldset>
+      </form>
+    </section>
+  );
+};
+
+export default Options;
--- src/components/Reps.tsx
+++ src/components/Reps.tsx
@@ -1,5 +1,34 @@
 const Reps = () => {
-    return <h2>Reps</h2>;
-  }
-  
-  export default Reps;
+  return (
+    <section>
+      <h2>Reps</h2>
+      <p>dynamically populate this later once there is a dataset. for now:</p>
+      <article className="rep_item">
+        <p>X number of ...</p>
+        <p>workout</p>
+      </article>
+      <article className="rep_item">
+        <p>X number of ...</p>
+        <p>workout</p>
+      </article>
+      <article className="rep_item">
+        <p>X number of ...</p>
+        <p>workout</p>
+      </article>
+      <article className="rep_item">
+        <p>X number of ...</p>
+        <p>workout</p>
+      </article>
+      <article className="rep_item">
+        <p>X number of ...</p>
+        <p>workout</p>
+      </article>
+      <article className="rep_item">
+        <p>X number of ...</p>
+        <p>workout</p>
+      </article>
+    </section>
+  );
+};
+
+export default Reps;
