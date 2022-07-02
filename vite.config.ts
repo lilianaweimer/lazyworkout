c5435d59e7fe4bded639244e99e09fe3f61acab3
Liliana Weimer <58522345+lilianaweimer@users.noreply.github.com>
2022-07-01 18:59:46 -0600

    hopefully config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lazy-workout/",
+  base: "/lazyworkout/",
   plugins: [react()],
 });
